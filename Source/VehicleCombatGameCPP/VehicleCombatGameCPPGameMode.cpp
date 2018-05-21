46ba6ea1542c914f3af732eb31faadd2c6d658fa
Need <needcoding@gmail.com>
2018-05-21 04:31:11 +0200

    - Moved original DrivePawn code to Base_DrivePawn

--- Source/VehicleCombatGameCPP/VehicleCombatGameCPPGameMode.cpp
+++ Source/VehicleCombatGameCPP/VehicleCombatGameCPPGameMode.cpp
@@ -1,11 +1,10 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #include "VehicleCombatGameCPPGameMode.h"
-#include "VehicleCombatGameCPPPawn.h"
+#include "Base/Base_DrivePawn.h"
 #include "VehicleCombatGameCPPHud.h"
 
-AVehicleCombatGameCPPGameMode::AVehicleCombatGameCPPGameMode()
-{
-	DefaultPawnClass = AVehicleCombatGameCPPPawn::StaticClass();
-	HUDClass = AVehicleCombatGameCPPHud::StaticClass();
+AVehicleCombatGameCPPGameMode::AVehicleCombatGameCPPGameMode() {
+  DefaultPawnClass = ABase_DrivePawn::StaticClass();
+  HUDClass = AVehicleCombatGameCPPHud::StaticClass();
 }
